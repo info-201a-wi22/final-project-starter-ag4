b4715c104f52ba30053814ea146cb27af7e0968d
nonzinv <nonzinv@uw.edu>
2022-02-17 13:44:27 -0800

    Non

--- source/summary_table.R
+++ source/summary_table.R
@@ -1,6 +1,7 @@
 library(dplyr)
-all_music <- read.csv("all_music.csv") 
+all_music <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-ag4/main/data/all_music.csv") 
 summary_table <- all_music %>%
   group_by(decade = all_music$decade) %>%
   summarize(bpm = mean(bpm),energy = mean(nrgy),dance = mean(dnce),decibels = mean(dB),duration = mean(dur)) %>% 
   round(digit = 2)
+
