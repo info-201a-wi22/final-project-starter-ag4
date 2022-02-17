7e01c0e660db6b98b873862756270e9d0a440dc9
cgraham24 <cag6082@uw.edu>
2022-02-17 13:32:32 -0800

    sum table

--- data/summary_table.R
+++ data/summary_table.R
@@ -2,5 +2,5 @@ library(dplyr)
 all_music <- read.csv("all_music.csv") 
 summary_table <- all_music %>%
   group_by(decade = all_music$decade) %>%
-  summarize(bpm = mean(bpm),energy = mean(nrgy),dance = mean(dnce),decibels = mean(dB),duration = mean(dur)) %>% 
+  summarize(mean_bpm = mean(bpm), median_bpm = median(bpm) ,mean_energy = mean(nrgy),median_energy = median(nrgy), mean_dance = mean(dnce), median_dance = median(dnce), mean_decibels = mean(dB), median_decibels = median(dB), mean_duration = mean(dur), median_duration = median(dur)) %>% 
   round(digit = 2)
