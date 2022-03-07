90d6dace810369d4c7134cb80608b7176e502b30
JakeW03 <jwaltzer@uw.edu>
2022-03-07 14:48:53 -0800

    no push3

--- p03-deliverable/P03-Deliverable/server.R
+++ p03-deliverable/P03-Deliverable/server.R
@@ -5,19 +5,37 @@ library(styler)
 library(tidyverse)
 library(shiny)
 
-music
-#all_music <- all_music %>% 
- # mutate(decade = floor(year/10)*10)
 
 
-chart_datax <- all_musicx %>%
+music_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-ag4/main/data/all_music.csv")
+
+
+graph_non <- music_data %>%
+  select(decade, nrgy, dnce, bpm) %>%
+  group_by(decade) %>%
+  summarize("Energy" = mean(nrgy), "Danceability" = mean(dnce), "BPM" = mean(bpm)) %>%
+  remove_rownames() %>%
+  column_to_rownames(var = "decade")
+  
+chart_data_jake <- music_data %>%
   select(year,dur,top.genre)%>%
   rename("Genre" = "top.genre")
-  #filter(Genre != is.null(TRUE))
+#filter(Genre != is.null(TRUE))
 
 server <- function(input, output) {
-  output$chartx <- renderPlotly({
-    plot_data <- chart_datax %>%
+  output$dataplot <- renderPlot({
+    barplot(graph_non[,input$type],
+            names = rownames(graph_non),
+            main = input$type,
+            ylim = c(0, 150),
+            col = "#69b3a2",
+            ylab = "Value",
+            xlab = "Type")
+  })
+  
+  
+  output$chart <- renderPlotly({
+    plot_data <- chart_data_jake %>%
       filter(Genre==input$genre)%>%
       select(year,dur)
     
@@ -31,31 +49,5 @@ server <- function(input, output) {
         y="Duration of Song (Seconds)",
         title ="Duration of Songs Over Years by Genre"
       )
-    
-  })
-  
-  
-}
-
-music_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-ag4/main/data/all_music.csv")
-
-
-graph_non <- music_data %>%
-  select(decade, nrgy, dnce, bpm) %>%
-  group_by(decade) %>%
-  summarize("Energy" = mean(nrgy), "Danceability" = mean(dnce), "BPM" = mean(bpm)) %>%
-  remove_rownames() %>%
-  column_to_rownames(var = "decade")
-  
-
-server <- function(input, output) {
-  output$dataplot <- renderPlot({
-    barplot(graph_non[,input$type],
-            names = rownames(graph_non),
-            main = input$type,
-            ylim = c(0, 150),
-            col = "#69b3a2",
-            ylab = "Value",
-            xlab = "Type")
   })
 }
